e5d0d5d51965dc817d02b49f5bddd2418703df8c
npalmer1 <ntpalmer1@yahoo.co.uk>
2025-12-31 15:16:35 +0000

    WS_Map016

--- MessageForm.cs
+++ MessageForm.cs
@@ -31,6 +31,7 @@ namespace WSPR_Map
 
         private void MessageForm_Load(object sender, EventArgs e)
         {
+            this.BringToFront();
             if (editbuttons)
             {
                 OKbutton.Visible = false;
